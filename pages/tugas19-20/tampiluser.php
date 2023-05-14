bf802d7378bedd4d210a5738325d87791adccf26
ernawati515 <ernaw3321@gmail.com>
2023-05-14 17:37:41 +0800

    ready

--- pages/tugas19-20/tampiluser.php
+++ pages/tugas19-20/tampiluser.php
@@ -1,6 +1,10 @@
 <?php
  $conn = mysqli_connect('localhost','root','','dump-ta_magang-202304021340');
- 
+ session_start();
+ if (isset($_SESSION["USER"]) == ""){
+     header("Location:login.php");
+     die;
+ }
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -8,12 +12,16 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Tugas19-User</title>
+    <title>User/Pengguna</title>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
 </head>
 <body>
-<h1>Tabel Pengguna</h1>
+<h1>Tabel USER/Pengguna</h1>
+<p><b>USER/Pengguna : <?php echo $_SESSION["USER"]?></p>
+<a class="btn btn-danger" href="logout.php">Log Out</a>
+<br>
+<br>
 <table class="table">
     <form action="">
                 <a class="btn btn-info" href="input.php">Tambah Pengguna</a>
